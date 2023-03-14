05831ad6ee98dd693192c1b331e7bf965338d7d4
fransedor <fransedoravelino@gmail.com>
2023-03-14 20:32:46 +0700

    added port to prod BE_URL

--- Config.ts
+++ Config.ts
@@ -1,1 +1,1 @@
-export const BE_URL = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/loocale' : "https://api.loocale.id/api/loocale"
+export const BE_URL = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/loocale' : "https://api.loocale.id/api/loocale:8080"
