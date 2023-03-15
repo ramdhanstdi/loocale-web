0804b69401eb4c574388bad43258c9b0a67ee3da
Fransedo Ravelino <88823714+fransedor@users.noreply.github.com>
2023-03-15 12:41:22 +0700

    Update Config.ts

--- Config.ts
+++ Config.ts
@@ -1,1 +1,1 @@
-export const BE_URL = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/loocale' : "https://api.loocale.id/api/loocale:8080"
+export const BE_URL = process.env.NODE_ENV === "development" ? 'http://localhost:5000/api/loocale' : "https://api.loocale.id:8080/api/loocale"
