10dbd392527a2812aec59b86ecd6258dc3554446
fransedor <fransedoravelino@gmail.com>
2023-03-26 20:32:58 +0700

    fixed fullsize picture

--- src/components/view/TimelineView/Post/components/PostPictureContainer.tsx
+++ src/components/view/TimelineView/Post/components/PostPictureContainer.tsx
@@ -36,22 +36,14 @@ const PostPictureContainer: React.FC<PostPictureContainerProps> = ({ medias }) =
                 loader={() => image.media_url}
                 width={216}
                 height={144}
-                unoptimized
                 alt="post-image"
-                className="rounded-md"
+                className="rounded-md object-cover"
               />
             </div>
             <Modal open={openFullsizePicture} onClose={() => setOpenFullsizePicture(false)}>
 							<CloseIcon className={"scale-[2] absolute top-6 right-6 rounded-full bg-white hover:cursor-pointer"}></CloseIcon>
-              <div className="w-3/4 h-3/4 relative top-1/2 -translate-y-1/2 mx-auto">
-                <Image
-                  src={fullsizePictureUrl}
-                  loader={() => fullsizePictureUrl}
-                  layout="fill"
-                  unoptimized
-                  alt="post-image"
-                  className="object-cover"
-                />
+              <div className="w-full h-full relative flex items-center justify-center mx-auto">
+                <img src={fullsizePictureUrl} alt="" className="max-w-screen max-h-screen mx-auto p-10" />
               </div>
             </Modal>
           </>
