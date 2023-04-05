981c44e465e3471cf88a4738ff8bff218a2c0672
fransedor <fransedoravelino@gmail.com>
2023-04-05 20:52:27 +0700

    added signout for desktop

--- src/components/view/TimelineView/LeftPanel/index.tsx
+++ src/components/view/TimelineView/LeftPanel/index.tsx
@@ -7,10 +7,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import CreatePostDialog from "../CreatePostDialog";
-import { getCurrentUser } from "src/utils/helper";
-import { UserDataInterface } from "src/models/Timeline";
-import sampleUser from "src/utils/sample";
 import { useGetUser } from "src/services/Timeline";
+import MoreIcon from "@icons/more_vert_icon.svg";
 
 const LEFT_PANEL_MENU = [
   {
@@ -33,6 +31,7 @@ const LEFT_PANEL_MENU = [
 const LeftPanel = () => {
   const router = useRouter();
   const [openCreatePost, setOpenCreatePost] = useState(false);
+  const [openProfileMenu, setOpenProfileMenu] = useState(false);
 
   const { data: currentUser } = useGetUser();
 
@@ -45,14 +44,17 @@ const LeftPanel = () => {
   } else {
     return (
       <>
-        <div className="flex flex-col justify-between my-10 ml-10">
+        <div
+          className="flex flex-col justify-between my-10 ml-10"
+          onClick={() => setOpenProfileMenu(false)}
+        >
           <div className="px-[30px] py-9 rounded-xl shadow-md flex flex-col gap-12">
             {LEFT_PANEL_MENU.map((menu) => (
               <a href={menu.href} key={menu.id}>
                 <div
                   className={`${
                     menu.href === router.pathname ? "text-secondary-500" : "text-primary-800"
-                  } ${menu.href === "#" ? "cursor-not-allowed" : "cursor-pointer" } `}
+                  } ${menu.href === "#" ? "cursor-not-allowed" : "cursor-pointer"} `}
                 >
                   {<menu.icon />}
                 </div>
@@ -62,22 +64,45 @@ const LeftPanel = () => {
               <AddIcon />
             </div>
           </div>
-          <div className="py-3 px-5 shadow-md rounded-lg flex flex-col ">
+          <div className="py-3 px-5 shadow-md rounded-lg flex flex-col relative">
+            <MoreIcon
+              className="absolute top-3 right-2 bg-gray-100 rounded-full hover:bg-gray-200 hover:cursor-pointer"
+              width={16}
+              height={16}
+              onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+                e.stopPropagation();
+                setOpenProfileMenu(true);
+              }}
+            />
+            {/* Profile Menu */}
+            <div className={`absolute z-10 bg-white bottom-20 left-20 text-xs items-center h-[180px] justify-between rounded-lg border border-primary-100 w-[140px] ${openProfileMenu ? "flex" : "hidden"} flex-col`}>
+              <div className="w-full text-center cursor-not-allowed">
+                <p className="font-bold py-2 border-b border-primary-100">Profil Saya</p>
+                <p className="font-bold py-2 border-b border-primary-100 ">Pengaturan</p>
+              </div>
+              <Link href={"/signin"}>
+                <p className="font-bold py-2 text-white bg-secondary-500 w-full text-center rounded-b-lg cursor-pointer">
+                  SIGN OUT
+                </p>
+              </Link>
+            </div>
             <div className="rounded-full mb-3 flex justify-center">
               {!currentUser.users.thumbnail ? (
-								<PeopleIcon></PeopleIcon>
-							): (
-								<Image
-								src={currentUser.users.thumbnail}
-								loader={() => currentUser.users.thumbnail!}
-								width={40}
-								height={40}
-								className="rounded-full"
-								alt="profile pic"
-								/>
-							)}
+                <PeopleIcon></PeopleIcon>
+              ) : (
+                <Image
+                  src={currentUser.users.thumbnail}
+                  loader={() => currentUser.users.thumbnail!}
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                  alt="profile pic"
+                />
+              )}
             </div>
-            <p className="text-secondary-500 text-[9px] text-center">{currentUser.users.user_name || ""}</p>
+            <p className="text-secondary-500 text-[9px] text-center">
+              {currentUser.users.user_name || ""}
+            </p>
           </div>
         </div>
         <CreatePostDialog open={openCreatePost} onClose={handleCloseDialog} />
