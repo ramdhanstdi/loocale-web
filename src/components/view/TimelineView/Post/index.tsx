81c84959d78bfdaf5884a0bad0c09204e9633792
fransedor <fransedoravelino@gmail.com>
2023-04-27 16:57:09 +0700

    added partner flag to posts

--- src/components/view/TimelineView/Post/index.tsx
+++ src/components/view/TimelineView/Post/index.tsx
@@ -9,7 +9,8 @@ import { PostDataInterface } from "src/models/Timeline";
 import Comment from "./components/Comments";
 import AddComment from "./components/AddComment";
 import { useGetUser } from "src/services/Timeline";
-import { useRouter } from "next/router";
+import WhatsAppIcon from "@icons/whatsapp_green_icon.svg";
+import InternetIcon from "@icons/internet_icon.svg";
 
 const Post: React.FC<PostDataInterface> = ({
   User,
@@ -53,14 +54,14 @@ const Post: React.FC<PostDataInterface> = ({
   //} else {
   return (
     <div
-      className="w-full py-4 sm:pl-[131px] pr-4 pl-16 border-b-[0.5px] border-b-primary-100 relative"
+      className="w-full py-4 sm:pl-[40px] pr-4 pl-4 border-b-[0.5px] border-b-primary-100 relative"
       onClick={() => {
-				setOpenPostMenu(false)
-			}}
+        setOpenPostMenu(false);
+      }}
     >
-      <div className="max-w-[600px] mx-auto relative">
-        <div className="absolute text-center top-0 sm:-left-20 -left-12 text-xs text-primary-200 font-light">
-          <div className="rounded-full flex justify-center">
+      <div className=" relative flex gap-5 px-auto max-w-[800px] mx-auto">
+        <div className="text-center text-xs text-primary-200 font-light">
+          <div className="rounded-full flex flex-col items-center justify-start w-10 gap-2">
             {User.thumbnail ? (
               <Image
                 src={User.thumbnail}
@@ -72,74 +73,81 @@ const Post: React.FC<PostDataInterface> = ({
             ) : (
               <PeopleIcon />
             )}
+            <p className="text-white px-1 bg-primary-800 font-bold leading-[18px]">Partner</p>
+            <div className="flex items-center justify-center gap-1">
+              <WhatsAppIcon className={"text-green-400"}/>
+              <InternetIcon />
+            </div>
           </div>
         </div>
-        <PostHeader
-          full_name={User.full_name}
-          createdAt={createdAt}
-          location={location}
-					postId={id}
-          location_detail={location_detail}
-          postingUser={User}
-          openPostMenu={openPostMenu}
-          setOpenPostMenu={setOpenPostMenu}
-        />
-        <p className="font-bold text-secondary-500 text-xs my-1">@{User.user_name}</p>
-        <p className="text-xs font-light text-justify break-words whitespace-pre-wrap mb-2">
-          {postText.length < 150 ? (
-            postText
-          ) : showMore ? (
-            <>
-              {postText}
-              {"  "}
-              <span
-                className="text-secondary-500 hover:underline hover:cursor-pointer font-light text-xs"
-                onClick={() => setShowMore(false)}
-              >
-                See less
-              </span>
-            </>
-          ) : (
-            <>
-              {postText.substring(0, 150)}
-              {"  "}
-              <span
-                className="text-secondary-500 hover:underline hover:cursor-pointer font-light text-xs"
-                onClick={() => setShowMore(true)}
-              >
-                See more
-              </span>
-            </>
-          )}
-          {}
-        </p>
-        <PostPictureContainer medias={medias} />
-        <PostInteractions
-          likes={Likes}
-          commentsCount={Comments.length}
-          setShowAddComment={setShowAddComment}
-          categories={Categories}
-          postId={String(id)}
-        />
-        {currentUser && showAddComment && <AddComment user={currentUser.users} postId={id} />}
-        {displayedComments.map((comment) => (
-          <Comment
-            key={comment.id}
-            commentText={comment.commentText}
-            user={comment.User}
-            createdAt={comment.createdAt}
+        <div className="flex flex-col grow">
+          <PostHeader
+            full_name={User.full_name}
+            createdAt={createdAt}
+            location={location}
+            postId={id}
+            location_detail={location_detail}
+            postingUser={User}
+            openPostMenu={openPostMenu}
+            setOpenPostMenu={setOpenPostMenu}
           />
-        ))}
-        {Comments.length > 1 ? (
-          <p
-            className="mt-3 text-xs font-light hover:underline hover:cursor-pointer"
-            onClick={loadMoreComments}
-          >
-            Lihat komentar ({Comments.length - displayedComments.length})
+          <p className="font-bold text-secondary-500 text-xs my-1">@{User.user_name}</p>
+          <p className="text-xs font-light text-justify break-words whitespace-pre-wrap mb-2">
+            {postText.length < 150 ? (
+              postText
+            ) : showMore ? (
+              <>
+                {postText}
+                {"  "}
+                <span
+                  className="text-secondary-500 hover:underline hover:cursor-pointer font-light text-xs"
+                  onClick={() => setShowMore(false)}
+                >
+                  See less
+                </span>
+              </>
+            ) : (
+              <>
+                {postText.substring(0, 150)}
+                {"  "}
+                <span
+                  className="text-secondary-500 hover:underline hover:cursor-pointer font-light text-xs"
+                  onClick={() => setShowMore(true)}
+                >
+                  See more
+                </span>
+              </>
+            )}
+            {}
           </p>
-        ) : (
-          <></>
-        )}
+          <PostPictureContainer medias={medias} />
+          <PostInteractions
+            likes={Likes}
+            commentsCount={Comments.length}
+            setShowAddComment={setShowAddComment}
+            categories={Categories}
+            postId={String(id)}
+          />
+          {currentUser && showAddComment && <AddComment user={currentUser.users} postId={id} />}
+          {displayedComments.map((comment) => (
+            <Comment
+              key={comment.id}
+              commentText={comment.commentText}
+              user={comment.User}
+              createdAt={comment.createdAt}
+            />
+          ))}
+          {Comments.length > 1 ? (
+            <p
+              className="mt-3 text-xs font-light hover:underline hover:cursor-pointer"
+              onClick={loadMoreComments}
+            >
+              Lihat komentar ({Comments.length - displayedComments.length})
+            </p>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );
